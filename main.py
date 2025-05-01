987df6137e93787659c279a6809ff7697463b85b
CC16-Oracle <lcstompkins@gmail.com>
2025-05-01 17:42:58 -0400

    Added player drawing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from constants import *
+from circleshape import CircleShape
+from player import Player
 
 def main():
     pygame.init()
@@ -11,13 +13,15 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return      
         screen.fill((0, 0, 0))
+        player.draw(screen)
         dt = clock.tick(60) / 1000
 
         pygame.display.flip()
