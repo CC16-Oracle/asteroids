f385070d12ec6bb457878f0799aea8ba8aece9f1
CC16-Oracle <lcstompkins@gmail.com>
2025-05-02 01:37:22 -0400

    Added Player Movement

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 import pygame
+
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from constants import *
+
 from circleshape import CircleShape
+
 from player import Player
 
 def main():
     pygame.init()
-   
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return      
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        
         dt = clock.tick(60) / 1000
+        updatable.update(dt)
 
+        for sprite in updatable:
+            sprite.draw(screen)
         pygame.display.flip()
 
 
